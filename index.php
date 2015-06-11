cfc3c6f6a226baa3fe7e31d2fa7d65b2417987bd
Yuji Suzuki <y-suzuki@zeneration.jp>
2015-06-11 16:30:53 +0900

    no message

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<aaiu>
-korehahidoi
+<html lang="ja">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>MCBIテストページ</title>
+	</head>
+	<body>
+		<div class="container">
+			<div id="tablemain">
+				<div class="box">
+					<div class="row1">a</div><div class="row2">a</div>
+				</div>
+				<div class="box">
+					<div class="row1">a</div><div class="row2">a</div>
+				</div>
+				<div class="box">
+					<div class="row1">a</div><div class="row2">a</div>
+				</div>
+				<div class="lastbox">
+					<div class="row1">a</div><div class="row2">a</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
